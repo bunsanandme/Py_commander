b4d5b352915dbf73a74ed875c21adf5eb02f8f30
Banya Delov <super.iwanttodie@yandex.ru>
2021-03-04 04:50:31 +0300

    Fix some bugs, add logs and icons

---
+++ styleSheet.py
@@ -0,0 +1,27 @@
+style = """
+        QWidget {
+            background-color: white;
+        }
+
+        QPushButton {
+            background-color: #ffbcad;
+            color: #33262c;
+            font-family: Consolas;
+            font-size: 15px;
+        }
+
+        QMenuBar {
+            background-color: #ffbcad;
+            background-color: #ffbcad;
+            color: #33262c;
+            font-family: Consolas;
+            font-size: bold 14px;
+        }
+
+        QMenuBar::item {
+            background-color: #ffbcad;
+            color: #33262c;
+            font-family: Consolas;
+            font-size:  14px;
+        }
+"""
